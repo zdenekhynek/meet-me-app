cb813f4cba2a4972cb29101ef00db30d5bf2d7ee
Zdenek Hynek <zdenek.hynek@gmail.com>
2020-06-12 15:13:26 +0100

    Basic input fetching

--- App.js
+++ App.js
@@ -1,37 +1,89 @@
-import React from "react";
-import { Image, StyleSheet, Text, TouchableOpacity, View, SafeAreaView, Dimensions } from "react-native";
+import React, { useState, useCallback, useEffect } from "react";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  SafeAreaView,
+  Dimensions,
+} from "react-native";
 import GooglePlacesInput from "./GooglePlacesInput";
-import MapView from 'react-native-maps';
+import MapView from "react-native-maps";
+
+export const getJourneyApiUrl = (hostUrl, fromString = "0,0", toString = "0,0") => {
+  const endpoint = "api/v1/directions";
+  return `${hostUrl}/${endpoint}/${fromString}/${toString}`;
+};
+
+export const fetchJourney = async () => {
+  const url = getJourneyApiUrl(process.env.REACT_NATIVE_APP_JOURNEY_API_HOST);
+  console.log('fetchingJourney from url', url);
+  const result = await fetch(url);
+  console.log('result', JSON.stringify(result));
+  
+  if (result.ok) {
+    return await result.json();
+  } else {
+    console.error("Error fetching journey");
+  }
+};
 
 export default function App() {
+  const [from, setFrom] = useState("0,0");
+  const [to, setTo] = useState("0,0");
+
+  const getJourneys = useCallback(async () => {
+    const journeys = await fetchJourney();
+    console.log('journeys', journeys);
+  }, []);
+
+  useEffect(() => {
+    if (from && to) {
+      console.log("fetching journey");
+      getJourneys(from, to)
+    }
+  }, [from, to]);
+
+  const handleFromChange = useCallback((value) => {
+    console.log("handleFromChange");
+    setFrom(value);
+  });
+  const handleToChange = useCallback((value) => {
+    console.log("handleToChange");
+    setTo(value);
+  });
+
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
+    <View style={styles.container}>
+      <SafeAreaView>
+        <Text>{process.env.REACT_NATIVE_APP_JOURNEY_API_HOST}</Text>
         <View style={styles.input}>
           <GooglePlacesInput
             placeholder="Place A"
             apiKey={process.env.REACT_NATIVE_GOOGLE_MAPS_API_KEY}
+            onChange={handleFromChange}
           />
         </View>
         <View style={styles.input}>
           <GooglePlacesInput
             placeholder="Place B"
             apiKey={process.env.REACT_NATIVE_GOOGLE_MAPS_API_KEY}
+            onChange={handleToChange}
           />
         </View>
-        <View style={styles.container}>
-          <MapView style={styles.mapStyle} />
-        </View>
-      </View>
-    </SafeAreaView>
+        <MapView style={styles.mapStyle} />
+      </SafeAreaView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
-  input: {
-    backgroundColor: "#f00",
+  container: {
+    flex: 1,
+    backgroundColor: "gray",
   },
+  input: {},
   instructions: {
     color: "#888",
     fontSize: 18,
@@ -39,7 +91,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   mapStyle: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height - 100,
   },
 });
--- GooglePlacesInput.js
+++ GooglePlacesInput.js
@@ -1,24 +1,34 @@
-import React from "react";
-import { Image, Text } from "react-native";
+import React, { useState } from "react";
+import { Image, Text, TextInput } from "react-native";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
-const GooglePlacesInput = ({ apiKey, placeholder }) => {
+const GooglePlacesInput = ({ apiKey, placeholder, onChange }) => {
+  const [value, setValue] = useState('Useless Placeholder');
+
   return (
-    <GooglePlacesAutocomplete
-      placeholder={placeholder}
-      minLength={2}
-      debounce={200}
-      fetchDetails={false}
-      onPress={(data, details = null) => {
-        // 'details' is provided when fetchDetails = true
-        console.log(data, details);
+    <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => {
+        setValue(text);
+        onChange(text);
       }}
-      currentLocation={true}
-      query={{
-        key: apiKey,
-        language: "en",
-      }}
+      value={value}
     />
+    // <GooglePlacesAutocomplete
+    //   placeholder={placeholder}
+    //   minLength={2}
+    //   debounce={200}
+    //   fetchDetails={false}
+    //   onPress={(data, details = null) => {
+    //     // 'details' is provided when fetchDetails = true
+    //     console.log(data, details);
+    //   }}
+    //   currentLocation={true}
+    //   query={{
+    //     key: apiKey,
+    //     language: "en",
+    //   }}
+    // />
   );
 };
 
